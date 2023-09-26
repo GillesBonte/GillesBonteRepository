b3e1d3537142b1f82299d1d4ffae2231d5c3ab92
GillesBonte <gilles.bonte1@telenet.be>
2023-09-26 19:42:11 +0200

    added AnimalErrorHandling

---
+++ AnimalErrorHandling/AnimalErrorHandling/clsAnimal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalErrorHandling
+{
+    internal class Animal
+    {
+
+        private int _legCount;
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int Legcount 
+        { 
+            get
+            {
+                return _legCount;
+            }
+                
+          set
+            {
+                if (value <= 1)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    _legCount = value;
+                }
+            }
+        
+        }
+        public string Color { get; set; }
+
+        public int GetAge()
+        {
+            if (DateOfBirth.HasValue)
+            {
+                int intAge = DateTime.Now.Year - DateOfBirth.Value.Year;
+                if (DateTime.Now.DayOfYear < DateOfBirth.Value.DayOfYear)
+                {
+                    intAge--;
+                }
+                return intAge;
+            }
+            else
+            {
+                throw new Exception()
+            }
+            
+        }
+
+    }
+
+}
