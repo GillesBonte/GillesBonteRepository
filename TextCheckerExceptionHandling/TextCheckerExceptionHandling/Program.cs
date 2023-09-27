ef21ace748ffdaa935d8ad3af5db76511f1a80a9
GillesBonte <gilles.bonte1@telenet.be>
2023-09-27 21:04:45 +0200

    TextChecker UPDATE - added logfile

--- TextCheckerExceptionHandling/TextCheckerExceptionHandling/Program.cs
+++ TextCheckerExceptionHandling/TextCheckerExceptionHandling/Program.cs
@@ -1,26 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reflection.Metadata.Ecma335;
 using TextCheckerExceptionHandling;
 
 try
 {
     TextChecker tcText = new TextChecker("aF");
     TextChecker tcText2 = new TextChecker("a9");
-    TextChecker tcText3 = new TextChecker("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz");
+    TextChecker tcText3 = new TextChecker("abcdeddghijklmnopqrstuvwxyzabcdeddghijklmnopqrstuvwxyz");
+    tcText3.TextCheck();
 }
 catch (ContainsNumberException ex)
 {
     Console.WriteLine(ex.ToString());
-    throw new Exception("Crash in progam", ex);
+    CustomFileLog.AddErrorLog(ex.ToString());
+    //throw new Exception("Crash in progam", ex); --> the throw happens in clsTextChecker
 }
 catch (ContainsLetterFException ex)
 {
     Console.WriteLine(ex.ToString());
-    throw new Exception("Crash in progam", ex);
+    CustomFileLog.AddErrorLog(ex.ToString());
+
+    //throw new Exception("Crash in progam", ex); --> the throw happens in clsTextChecker
 }
 catch (ContainsAlotException ex)
 {
     Console.WriteLine(ex.ToString());
-    throw new Exception("Crash in progam", ex);
+    CustomFileLog.AddErrorLog(ex.ToString());
+    //throw new Exception("Crash in progam", ex); --> the throw happens in clsTextChecker
 }
+
+Console.ReadLine();
 
---
+++ TextCheckerExceptionHandling/TextCheckerExceptionHandling/clsCustomFileLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCheckerExceptionHandling
+{
+    public static class CustomFileLog
+    {
+        const string _logPath = @"C:\SyntraWestTemp\Logging";
+
+        public static void AddErrorLog(string message)
+        {
+            //Create directory if it does not exists
+            Directory.CreateDirectory(_logPath);
+            File.AppendAllText(_logPath + "/log.txt", string.Format("{0} - {1}" + Environment.NewLine, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff"), message + "\n"));
+        }
+    }
+}
