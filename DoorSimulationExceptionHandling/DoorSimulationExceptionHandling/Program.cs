a842650a52cf7004483e08a4d667cdea3a4db616
GillesBonte <gilles.bonte1@telenet.be>
2023-09-27 21:51:47 +0200

    added code to test LockDoor

--- DoorSimulationExceptionHandling/DoorSimulationExceptionHandling/Program.cs
+++ DoorSimulationExceptionHandling/DoorSimulationExceptionHandling/Program.cs
@@ -2,10 +2,17 @@
 
 try
 {
-    Door newDoor = new Door(false, true);
-    newDoor.OpenDoor();
+    //  Door newDoor = new Door(false, true);
+    //  newDoor.OpenDoor();
+    Door newDoor2 = new Door(true, false);
+    newDoor2.LockDoor();
 }
 catch (ExceptionDoorLocked ex)
+{
+    Console.WriteLine(ex);
+    CustomFileLog.AddErrorLog(ex.ToString());
+}
+catch (ExceptionDoorOpened ex)
 {
     Console.WriteLine(ex);
     CustomFileLog.AddErrorLog(ex.ToString());
