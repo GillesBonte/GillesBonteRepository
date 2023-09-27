527e9e7e98456425faafcd14a84d8b170cb3bcfe
GillesBonte <gilles.bonte1@telenet.be>
2023-09-27 21:13:42 +0200

    Created class 'Door'

---
+++ DoorSimulationExceptionHandling/DoorSimulationExceptionHandling/clsDoor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorSimulationExceptionHandling
+{
+    public class Door
+    {
+        public Door()
+        { 
+        }
+
+        public void LockDoor()
+        {
+
+        }
+
+        public void UnlockDoor()
+        {
+
+        }
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+
+    }
+
+}
