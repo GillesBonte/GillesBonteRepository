177e1a2724e0053ce8fe134697c126a301240b45
GillesBonte <gilles.bonte1@telenet.be>
2023-09-28 10:02:48 +0200

    created the class Laptop

---
+++ EventExerciseLaptop/EventExerciseLaptop/clsLaptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EventExerciseLaptop
+{
+    public class Laptop
+    {
+        public Laptop()
+        { 
+        }
+
+        public bool Powered { get; set; }
+
+        public void PowerOn()
+        {           
+        }
+        
+        public void PowerOff()
+        {
+        }
+
+    }
+
+}
+
