886fd1d1255b93560f44f84a9af260c8181dfac7
GillesBonte <gilles.bonte1@telenet.be>
2023-09-30 19:01:03 +0200

    added exercise CalculationProgressAsync

---
+++ CalculationProgressAsync/CalculationProgressAsync/frmDataProgress.cs
@@ -0,0 +1,15 @@
+namespace CalculationProgressAsync
+{
+    public partial class frmDataProgress : Form
+    {
+        public frmDataProgress()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
