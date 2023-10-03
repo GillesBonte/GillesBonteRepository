a0b87d983586daaa9240b5a89565a8a601c16503
GillesBonte <gilles.bonte1@telenet.be>
2023-10-03 20:35:39 +0200

    MyStack (complete)

---
+++ MyStackExerciseGenerics/MyStackExerciseGenerics/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using MyStackExerciseGenerics;
+
+MyStack<int> nieuweStack = new MyStack<int>();
+
+nieuweStack.Add(1);
+nieuweStack.Add(2);
+nieuweStack.Add(3);
+nieuweStack.Add(4);
+nieuweStack.Add(5);   
+nieuweStack.Add(6);
+nieuweStack.Add(7);
+nieuweStack.Add(8);
+nieuweStack.Add(9);
+nieuweStack.Add(10);
+try
+{
+    Console.WriteLine(nieuweStack.Peek());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+    Console.WriteLine(nieuweStack.Remove());
+}
+catch (InvalidOperationException ex)
+{
+
+    Console.WriteLine("Stack is empty");
+}
+
+
+Console.ReadLine();
---
+++ MyStackExerciseGenerics/MyStackExerciseGenerics/clsMyStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStackExerciseGenerics
+{
+    public class MyStack<T>
+    {
+        private T[] _items;
+
+        public MyStack()
+        {
+            _items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            int intLength = _items.Length + 1;
+            T[] newItems = new T[intLength];
+            _items.CopyTo(newItems, 0);
+            newItems[intLength - 1] = item;
+            _items = newItems;
+        }
+
+        public T Remove()
+        {
+            T result;
+
+            result = Peek();
+
+            T[] newItems = new T[_items.Length - 1];
+
+            for (int i = 0; i < _items.Length - 1; i++)
+            {
+                newItems[i] = _items[i];
+            }
+
+            _items = newItems;
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            if (_items.Length == 0)
+            {
+                throw new InvalidOperationException("ERROR");
+            }
+
+            return _items[_items.Length - 1];
+        }
+
+    }
+
+}
