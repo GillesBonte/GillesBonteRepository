9475266af8d830b95389fe4154a9c8f388c502e9
GillesBonte <gilles.bonte1@telenet.be>
2023-10-05 20:42:35 +0200

    added FilterByTypeExerciseGenerics

---
+++ FilterByTypeExerciseGenerics/FilterByTypeExerciseGenerics/Program.cs
@@ -0,0 +1,35 @@
+﻿List<T> FilterByType<T>(List<object> mixedList)
+{
+    List<T> result = new List<T>();
+
+    foreach (object item in mixedList)
+    {
+
+        if (item is T)
+        {
+            result.Add((T)item);
+        }
+
+    }
+
+    return result;
+}
+
+
+
+List<object> checkList = new List<object>();
+List<DateTime> resultList = new List<DateTime>();
+
+checkList.Add(1);
+checkList.Add("1");
+checkList.Add(DateTime.Now);
+
+resultList = FilterByType<DateTime>(checkList);
+
+foreach (object item in resultList) 
+{
+    Console.WriteLine(item.ToString());
+}
+
+
+Console.ReadLine();
