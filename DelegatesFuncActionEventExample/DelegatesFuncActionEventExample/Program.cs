a6a40828997155b020d2568d2452c14f26b864c4
GillesBonte <gilles.bonte1@telenet.be>
2023-10-08 18:33:24 +0200

    added Event example

---
+++ DelegatesFuncActionEventExample/DelegatesFuncActionEventExample/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var guild = new Guild();
+        var welcomer = new MemberWelcomer();
+        var hall = new GuildHall();
+
+        //public NONstatic events
+        //guild.NewMemberAdded += welcomer.WelcomeMember;
+        //guild.NewMemberAdded += hall.AddBedroom;
+
+        guild.AddMember("Gilles");
+    }
+
+}
+
+public class Guild
+{
+    private readonly List<string> _members = new List<string>();
+
+    //public NONstatic events
+    //public event Action<string> NewMemberAdded;
+
+    //public STATIC events
+    public static event Action<string> NewMemberAdded;
+
+    public void AddMember(string strMemberName)
+    {
+        _members.Add(strMemberName);
+        NewMemberAdded?.Invoke(strMemberName);
+    }
+
+}
+
+public class MemberWelcomer
+{
+    public MemberWelcomer() 
+    {
+        Guild.NewMemberAdded += WelcomeMember;
+    } 
+    public void WelcomeMember(string strMemberName)
+    {
+        Console.WriteLine($"Welcome {strMemberName}.");
+    }
+
+}
+
+public class GuildHall
+{
+    public void AddBedroom(string strMemberName)
+    {
+        Console.WriteLine($"A room was added to the guild hall for {strMemberName}");
+    }
+
+}
+
