4625ee0c5c6a277961d5041e1f06c54823093991
GillesBonte <gilles.bonte1@telenet.be>
2023-10-08 11:44:29 +0200

    added the DayCourses query

--- CourseExerciseLinq/CourseExerciseLinq/Program.cs
+++ CourseExerciseLinq/CourseExerciseLinq/Program.cs
@@ -5,11 +5,12 @@ using System.Runtime.CompilerServices;
 
 Courses courses = new Courses();
 IEnumerable<Course> queryResult;
-queryResult = MoreThanFifteen(courses);
+//queryResult = MoreThanFifteen(courses);
+queryResult = DayCourses(courses);
 
 foreach (Course course in queryResult)
 {
-    Console.WriteLine($"{course.Teacher}, {course.Room}, {course.StudentCount}");
+    Console.WriteLine($"{course.Teacher}\t{course.Room}\t{course.StudentCount}\t{course.Periode}");
 }
 
 Console.ReadLine();
@@ -20,6 +21,16 @@ static IEnumerable<Course> MoreThanFifteen(Courses courses)
     var result = from course in courses
              where course.StudentCount >= 15
              select course;
+
+    return result;
+}
+
+static IEnumerable<Course> DayCourses(Courses courses)
+{
+
+    var result = from course in courses
+                 where course.Periode.Equals(Course.Period.Day)
+                 select course;
 
     return result;
 }
