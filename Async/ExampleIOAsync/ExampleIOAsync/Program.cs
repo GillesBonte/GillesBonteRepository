94ae14f743692025216fb23a9ec28f68b3e588b2
GillesBonte <gilles.bonte1@telenet.be>
2023-10-14 15:33:02 +0200

    added exampleIOAsync

---
+++ Async/ExampleIOAsync/ExampleIOAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExampleIOAsync
+{
+    internal class Program
+    {       
+        static async Task Main()
+        {
+            string result = await DownloadWebPageAsync("https://example.com");
+            Console.WriteLine($"Web Page Content: {result.Length} characters");
+        }
+
+        static async Task<string> DownloadWebPageAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(url);
+            }
+        }
+    }
+}
