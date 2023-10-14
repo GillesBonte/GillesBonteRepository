beeb385049e90fccba5f0ebac4a4cde67f2808ef
GillesBonte <gilles.bonte1@telenet.be>
2023-10-14 15:24:11 +0200

    added folder structure added asyncCPUexample

---
+++ Async/ExampleCPUAsync/ExampleCPUAsync/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExampleCPUAsync
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            int result = await ComputeFactorialAsync(15);
+            Console.WriteLine($"Factorial: {result}");
+        }
+
+        static async Task<int> ComputeFactorialAsync(int n)
+        {
+            return await Task.Run(() => Factorial(n));
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
