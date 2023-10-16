f788b55e7be25aa55969e09d5c9f30d2270c7465
GillesBonte <gilles.bonte1@telenet.be>
2023-10-16 09:11:21 +0200

    example exceptions working with files

---
+++ ExceptionHandling/DirectoryExampleExceptionHandling/DirectoryExampleExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DirectoryExampleExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //the file-property "Copy to Output Directory" needs to be enabled (copy always)
+            var numbersList = File.ReadAllLines(Directory.GetCurrentDirectory() + "/Info.txt");
+            List<int> numbers = new List<int>();
+
+            try
+            {
+                foreach (var item in numbersList)
+                {
+                    numbers.Add(int.Parse(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ooops, something went wrong.");
+            }
+            finally
+            {
+                foreach(var item in numbers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+
+
+
+        }
+    }
+}
