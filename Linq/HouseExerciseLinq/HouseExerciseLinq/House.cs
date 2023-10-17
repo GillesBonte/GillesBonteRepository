7b307920bd624fa0651b9030b8737b76142f8bb7
GillesBonte <gilles.bonte1@telenet.be>
2023-10-17 08:00:28 +0200

    added class House to HouseExercise started on the ShowData method

---
+++ Linq/HouseExerciseLinq/HouseExerciseLinq/House.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseExerciseLinq
+{
+    public class House
+    {
+        public enum Type
+        {
+            Villa,
+            Bungalow,
+            Farm,
+            Cottage
+        }
+
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public decimal Price { get; set; }
+        public int BedroomCount { get; set; }
+        public decimal Squaremeter { get; set; }
+        public Type TypeBuilding { get; set; }
+
+    }
+
+}
+
--- Linq/HouseExerciseLinq/HouseExerciseLinq/Program.cs
+++ Linq/HouseExerciseLinq/HouseExerciseLinq/Program.cs
@@ -2,9 +2,60 @@
 {
     internal class Program
     {
+        private static List<House> houses = new List<House>();
         static void Main(string[] args)
         {
+            CreateData();
+            ShowFilteredData();
+        }
 
+        static void CreateData()
+        {
+            houses.Add(new House()
+            {
+                Name = "MyFirstHouse",
+                Region = "WestFlanders",
+                Price = 244000,
+                BedroomCount = 2,
+                Squaremeter = 125,
+                TypeBuilding = House.Type.Cottage
+            });
+            houses.Add(new House()
+            {
+                Name = "MySecondHouse",
+                Region = "WestFlanders",
+                Price = 340000,
+                BedroomCount = 3,
+                Squaremeter = 150,
+                TypeBuilding = House.Type.Villa
+            });
+            houses.Add(new House()
+            {
+                Name = "MyThirdHouse",
+                Region = "EastFlanders",
+                Price = 320000,
+                BedroomCount = 4,
+                Squaremeter = 200,
+                TypeBuilding = House.Type.Farm
+            });
+            houses.Add(new House()
+            {
+                Name = "MyFourthHouse",
+                Region = "Brussels",
+                Price = 400000,
+                BedroomCount = 3,
+                Squaremeter = 125,
+                TypeBuilding = House.Type.Bungalow
+            });
+            houses.Add(new House()
+            {
+                Name = "MyFirstHouse",
+                Region = "Brussels",
+                Price = 300000,
+                BedroomCount = 2,
+                Squaremeter = 100,
+                TypeBuilding = House.Type.Cottage
+            });
         }
 
     }
