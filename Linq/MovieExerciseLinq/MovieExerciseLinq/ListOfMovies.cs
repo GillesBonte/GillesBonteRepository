4e641c1e043fb5c6888a74e10bf0ea8102cdd648
GillesBonte <gilles.bonte1@telenet.be>
2023-10-17 12:36:44 +0200

    completed MovieExerciseLinq

---
+++ Linq/MovieExerciseLinq/MovieExerciseLinq/ListOfMovies.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExerciseLinq
+{
+    public class ListOfMovies : List<Movie>
+    {
+        //private List<Movie> _movies;
+        public ListOfMovies() 
+        { 
+            //_movies = new List<Movie>();
+
+            this.Add(new Movie()
+            {
+                Name = "Inception",
+                Rating = 6,
+                Producer = "Christopher Nolan",
+                Director = "Christopher Nolan",
+                ReleaseDate = DateTime.ParseExact("21/07/2010","dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+
+            this.Add(new Movie()
+            {
+                Name = "Alice in Wonderland",
+                Rating = 7,
+                Producer = "Joe Roth",
+                Director = "Tim Burton",
+                ReleaseDate = DateTime.ParseExact("05/03/2010", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+
+            this.Add(new Movie()
+            {
+                Name = "Jaws",
+                Rating = 5,
+                Producer = "Richard D. Zanuck",
+                Director = "Steven Spielberg",
+                ReleaseDate = DateTime.ParseExact("20/06/1975", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+
+            this.Add(new Movie()
+            {
+                Name = "E.T.",
+                Rating = 5,
+                Producer = "Steven Spielberg",
+                Director = "Steven Spielberg",
+                ReleaseDate = DateTime.ParseExact("26/05/1982", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+
+            this.Add(new Movie()
+            {
+                Name = "Super 8",
+                Rating = 4,
+                Producer = "J.J. Abrams",
+                Director = "J.J. Abrams",
+                ReleaseDate = DateTime.ParseExact("10/06/2011", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+
+            this.Add(new Movie()
+            {
+                Name = "The book of Eli",
+                Rating = 6,
+                Producer = "J.J. Abrams",
+                Director = "Joel Silver",
+                ReleaseDate = DateTime.ParseExact("15/01/2010", "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture)
+            });
+        }
+
+        //public List<Movie> Movies
+        //{
+        //    get
+        //    {
+        //        return _movies;
+        //    }
+        //    set
+        //    {
+        //        value = _movies;
+        //    }
+        //}
+    }
+}
--- Linq/MovieExerciseLinq/MovieExerciseLinq/Program.cs
+++ Linq/MovieExerciseLinq/MovieExerciseLinq/Program.cs
@@ -1,9 +1,78 @@
-﻿namespace MovieExerciseLinq
+﻿using System.Dynamic;
+
+namespace MovieExerciseLinq
 {
     internal class Program
     {
+        static ListOfMovies _movies = new ListOfMovies();
         static void Main(string[] args)
         {
+            
+            IEnumerable<string> result = new List<string>();
+
+            //result = GetAllTwentyTenMovies();
+            //result = RatingBetweenFourAndSix();
+            //result = StevenSpielbergMovies();
+            //result = FilmsPerDirector();
+            //result = FilmsPerProducer();
+            result = AverageRatingPerYear();
+
+            ShowResult(result);
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> GetAllTwentyTenMovies()
+        {
+            return from Movie in _movies
+                   where Movie.ReleaseDate.Year == 2010
+                   select $"{Movie.Name}\t{Movie.ReleaseDate.Date.Month}/{Movie.ReleaseDate.Date.Year}";
+        }
+
+        static IEnumerable<string> RatingBetweenFourAndSix()
+        {
+            return from Movie in _movies
+                   where Movie.Rating >= 4 && Movie.Rating <= 6
+                   select $"{Movie.Name}\t{Movie.Rating}";
+        }
+
+        static IEnumerable<string> StevenSpielbergMovies()
+        {
+            return from Movie in _movies
+                   where Movie.Director == "Steven Spielberg"
+                   select $"{Movie.Name}\t{Movie.Director}";
+        }
+
+        static IEnumerable<string> FilmsPerDirector()
+        {
+            return from Movie in _movies
+                   group Movie by Movie.Director into MoviesPerDirector
+                   select $"{MoviesPerDirector.Key}\t{MoviesPerDirector.Count()}".ToString();
+        }
+
+        static IEnumerable<string> FilmsPerProducer()
+        {
+            return from Movie in _movies
+                   group Movie by Movie.Producer into MoviesPerProducer
+                   select $"{MoviesPerProducer.Key}\t{MoviesPerProducer.Count()}".ToString();
+        }
+
+        static IEnumerable<string> AverageRatingPerYear()
+        {
+            return from Movie in _movies
+                   group Movie by Movie.ReleaseDate.Year into MoviePerYear
+                   //select $"{MoviePerYear.Key}\t{MoviePerYear.Count()}";
+            select
+            
+                $"Year = {MoviePerYear.Key}\tAverageRating = {MoviePerYear.Average(movie => movie.Rating)}"
+            ;
+        }
+
+        static void ShowResult(IEnumerable<string> result)
+        {
+            foreach (string item in result) 
+            {
+                Console.WriteLine(item);
+            }
         }
 
     }
