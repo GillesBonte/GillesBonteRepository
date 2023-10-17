99405c846b78f8dec896b19f32377d4c258bcf86
GillesBonte <gilles.bonte1@telenet.be>
2023-10-17 11:25:47 +0200

    created MoviesExerciseLinq with class Movie

---
+++ Linq/MovieExerciseLinq/MovieExerciseLinq/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExerciseLinq
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public int Rating { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+    //Maak de klasse “Movie”
+    //Properties: Name, Producer, Rating, Director, ReleaseDate
+
+}
