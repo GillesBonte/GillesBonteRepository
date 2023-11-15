d1c446cc3411438d4c5d7b80eb6fdb04b34e95c1
GillesBonte <gilles.bonte1@telenet.be>
2023-11-15 20:43:53 +0100

    added class Spell

---
+++ Patterns/CardGameBehavioralPatterrn/CardGameBehavioralPatterrn/Models/Spell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameBehavioralPatterrn.Models
+{
+    public class Spell : Card
+    {
+        public Spell(int damage, int cost, string name)
+        {
+            Damage = damage;
+            Cost = cost;
+            Name = name;
+        }
+
+        public int Damage { get; set; }
+    }
+}
