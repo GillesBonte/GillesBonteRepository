1da7f6e52b805a416338d79d47a504ef8e2efb21
GillesBonte <gilles.bonte1@telenet.be>
2023-11-20 12:56:43 +0100

    added wroking Program

--- UnitTesting/DoorControl/DoorControl/Program.cs
+++ UnitTesting/DoorControl/DoorControl/Program.cs
@@ -1,15 +1,46 @@
 ﻿using DoorControlLogic;
+using System.Security.Cryptography;
+
 namespace DoorControl
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            LoadMenu();
+        }
+        static void LoadMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1) Start Toegangsvraag");
+            Console.WriteLine("2) Afsluiten");
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.KeyChar == 49)
+            {
+                ExecuteProgram();
+            }
+
+            if (key.KeyChar == 50)
+            {
+                Console.Clear();
+                Environment.Exit(0);
+            }
+
+        }
+        static void ExecuteProgram()
+        {
+            Console.Clear();
+            Console.Write("Enter TokenID: ");
+            string tokenId = Console.ReadLine();
+            Console.Write("Enter Doornumber: ");
+            int doorNumber = int.Parse(Console.ReadLine());
+
             int intResult;
             Validation validation = new Validation();
             try
             {
-                intResult = validation.GetStatus("B69G", 54);
+                intResult = validation.GetStatus(tokenId, doorNumber);
             }
             catch (ArgumentException)
             {
@@ -19,14 +50,8 @@ namespace DoorControl
 
             Console.WriteLine(intResult);
             Console.ReadLine();
+            LoadMenu();
         }
-
-        static void LoadMenu()
-        {
-            Console.WriteLine("1) Start Toegangsvraag");
-            Console.WriteLine("2) Afsluiten");
-        }
-
     }
 
 }
