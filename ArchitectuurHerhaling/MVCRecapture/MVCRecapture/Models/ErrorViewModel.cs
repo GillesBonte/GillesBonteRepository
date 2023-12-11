a630dc559057670792dfd647c8bd6a2045637823
GillesBonte <gilles.bonte1@telenet.be>
2023-12-11 19:14:14 +0100

    creation MVCRecapture

---
+++ ArchitectuurHerhaling/MVCRecapture/MVCRecapture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCRecapture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
